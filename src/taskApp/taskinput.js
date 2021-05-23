743d9c1aa2832a6c3f807b9410e8e9c4960580eb
Madhu <madhumitha4792@gmail.com>
2021-05-23 15:49:16 +0530

    Adding task evaluation

--- src/taskApp/taskinput.js
+++ src/taskApp/taskinput.js
@@ -1,12 +1,33 @@
-import { Component } from "react";
+import React, { Component } from "react";
+import $ from "jQuery";
 
 export class TaskInput extends Component {
+  constructor(props) {
+    super(props);
+    this.addTask = this.addTask.bind(this);
+    this.inputRef = React.createRef();
+  }
+  addTask() {
+    const inputVal = $(this.inputRef.current).val();
+    if (!!inputVal) {
+      alert("task added");
+    } else {
+      alert("text is empty");
+    }
+  }
   render() {
     return (
       <>
-        <text> Add your todo </text>
-        <input type="text" className="inputTask" placeholder="Add your task" />
-        <button className="addTask">Add todo </button>
+        Add your todo
+        <input
+          type="text"
+          ref={this.inputRef}
+          className="inputTask"
+          placeholder="Add your task"
+        />
+        <button className="addTask" onClick={this.addTask}>
+          Add todo
+        </button>
       </>
     );
   }
